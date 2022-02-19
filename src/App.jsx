cd0bc50dabd8a96b3f2e614d82f52f1bc02e2918
ehsan <ehsansaraee6570@yahoo.com>
2022-02-19 20:32:50 +0330

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css';
+import styled from 'styled-components';
+import { Navbar } from './components/navbar';
+
+const App = () => {
+   return (
+      <AppContainer>
+         <Navbar />
+         <InnerContainer>
+            <SomeContent>Some Content!</SomeContent>
+         </InnerContainer>
+      </AppContainer>
+   );
+};
+
+const AppContainer = styled.div`
+   width: 100%;
+   height: 100%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   background-color: rgba(15, 15, 15, 1);
+   color: #fff;
+`;
+
+const InnerContainer = styled.div`
+   width: 100%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   padding: 2em;
+`;
+
+const SomeContent = styled.h2`
+   color: #fff;
+`;
+
+export default App;
